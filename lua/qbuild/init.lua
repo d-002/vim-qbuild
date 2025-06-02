87410a978951bc4b5962a4500bcc5a13ed23739d
d-002 <minecraftenjoy@outlook.fr>
2025-06-03 00:06:41 +0200

    get root from project.nvim

--- lua/qbuild/init.lua
+++ lua/qbuild/init.lua
@@ -1,1 +1,19 @@
 print("Hello world")
+
+local M = {}
+
+function M.get_root()
+    local ok, project = pcall(require, "project_nvim.project")
+    if not ok then return vim.fn.getcwd() end
+
+    local config_ok, config = pcall(require, "project_nvim.config")
+    if config_ok and not config.config then
+        require("project_nvim").setup()
+    end
+
+    return project.get_project_root() or vim.fn.getcwd()
+end
+
+vim.keymap.set("n", "<leader>b", function() print(M.get_root()) end)
+
+return M
