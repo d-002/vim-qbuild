6111f30598fd61a8d1ce9e09e7471ba0a3f0da72
d-002 <minecraftenjoy@outlook.fr>
2025-06-03 00:45:07 +0200

    can open build directory, more checks and better API

--- lua/qbuild/init.lua
+++ lua/qbuild/init.lua
@@ -3,7 +3,7 @@ print("Hello world")
 local config = require("qbuild.config")
 local M = {}
 
-function M.get_root()
+function get_root()
     local ok, project = pcall(require, "project_nvim.project")
     if not ok then return vim.fn.getcwd() end
 
@@ -15,15 +15,13 @@ function M.get_root()
     return project.get_project_root() or vim.fn.getcwd()
 end
 
-function M.get_scripts_dir()
-    return vim.fs.joinpath(M.get_root(), config.options.build_dir)
+function get_scripts_dir()
+    return vim.fs.joinpath(get_root(), config.options.build_dir)
 end
 
 function M.run_nth_build_file(index)
     local i = 0;
-    local parent = M.get_scripts_dir();
-
-    print("ok")
+    local parent = get_scripts_dir();
 
     for name, type in vim.fs.dir(parent) do
         if type == "file" then
@@ -38,16 +36,31 @@ function M.run_nth_build_file(index)
         end
     end
 
-    print("Target build file could not be found")
+    print("Target qbuild file could not be found")
 
     return 1
+end
+
+function M.open_build_dir()
+    local path = get_scripts_dir()
+    local stat = vim.uv.fs_stat(path)
+
+    if not stat or stat.type ~= "directory" then
+        print("Inexistent or invalid qbuild directory")
+        return
+    end
+
+    vim.cmd("Vexplore " .. path)
 end
 
 function M.run_build_file()
     return M.run_nth_build_file(0)
 end
 
-vim.keymap.set("n", "<leader>t", function() print(M.get_root()) end)
-vim.keymap.set("n", "<leader>b", M.run_build_file)
+vim.keymap.set("n", "<leader>qb", M.run_build_file)
+vim.keymap.set("n", "<leader>q0", function() M.run_build_file(0) end)
+vim.keymap.set("n", "<leader>q1", function() M.run_build_file(1) end)
+vim.keymap.set("n", "<leader>q2", function() M.run_build_file(2) end)
+vim.keymap.set("n", "<leader>qo", M.open_build_dir)
 
 return M
